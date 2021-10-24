daaf63dff52f7ada49a1ac7c661d4ea65012e797
Ankushjare <jareankush@gmail.com>
2021-10-24 19:56:36 +0530

    Show Wining Position

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -4,12 +4,11 @@ namespace SnakeAndLadder
 {
     class Program
     {
-        const int START_POISTION = 0;
-        const int END_POSTION = 100;
+        const int START_POSITION = 0;
+        const int END_POSITION = 100;
         public static int currentPosition = 0;
         public static int selectedOption = 0;
         static Random random = new Random();
-
         public static int throwDice()
         {
             return random.Next(1, 7);
@@ -22,33 +21,35 @@ namespace SnakeAndLadder
 
         public static int playerOption()
         {
-            int throwDices = throwDice();
-            int selectedOption = checkForOption();
-            if (selectedOption == 1)
+            while (currentPosition < END_POSITION - 1)
             {
-                currentPosition += throwDices;
-            }
-            else if (selectedOption == 2)
-            {
-                currentPosition -= throwDices;
-
-            }
-            else
-            {
-                Console.WriteLine("Do nothing player stay in same position");
+                int throwDices = throwDice();
+                int selectedOption = checkForOption();
+                if (selectedOption == 1)
+                {
+                    currentPosition += throwDices;
+                }
+                else if (selectedOption == 2)
+                {
+                    currentPosition -= throwDices;
+                    if (currentPosition < 0)
+                    {
+                        currentPosition = START_POSITION;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Do nothing player stay in same poistion");
+                }
             }
             return currentPosition;
         }
 
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome message");
-            currentPosition += throwDice();
-            Console.WriteLine("start position is: " + START_POISTION);
-            Console.WriteLine("end position is: " + END_POSTION);
-            Console.WriteLine("current position is= " + playerOption());
+            Console.WriteLine("start postion is: " + START_POSITION);
+            Console.WriteLine("end postion is: " + END_POSITION);
+            Console.WriteLine("current poistion is= " + playerOption());
         }
-
     }
 }
