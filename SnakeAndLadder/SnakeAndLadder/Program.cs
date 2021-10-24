b749459662e08f40f16d2b919b190e49258984e7
Ankushjare <jareankush@gmail.com>
2021-10-24 19:54:14 +0530

    Check For Option

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -6,22 +6,48 @@ namespace SnakeAndLadder
     {
         const int START_POISTION = 0;
         const int END_POSTION = 100;
-        public static int currentPoistion = 0;
+        public static int currentPosition = 0;
+        public static int selectedOption = 0;
+        static Random random = new Random();
+
         public static int throwDice()
         {
-            Random random = new Random();
-            int number = random.Next(1, 7);
-            return number;
+            return random.Next(1, 7);
         }
+
+        public static int checkForOption()
+        {
+            return random.Next(1, 4);
+        }
+
+        public static int playerOption()
+        {
+            int throwDices = throwDice();
+            int selectedOption = checkForOption();
+            if (selectedOption == 1)
+            {
+                currentPosition += throwDices;
+            }
+            else if (selectedOption == 2)
+            {
+                currentPosition -= throwDices;
+
+            }
+            else
+            {
+                Console.WriteLine("Do nothing player stay in same position");
+            }
+            return currentPosition;
+        }
+
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome In SnekeAndLadder Game\n\n");
-            currentPoistion += throwDice();
-            Console.WriteLine("Current poistion is=" + currentPoistion);
-
-            Console.WriteLine("start postion is: " + START_POISTION);
-            Console.WriteLine("end postion is: " + END_POSTION);
+            Console.WriteLine("Welcome message");
+            currentPosition += throwDice();
+            Console.WriteLine("start position is: " + START_POISTION);
+            Console.WriteLine("end position is: " + END_POSTION);
+            Console.WriteLine("current position is= " + playerOption());
         }
 
     }
