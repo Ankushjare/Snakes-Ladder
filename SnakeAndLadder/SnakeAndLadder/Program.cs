e131864f2b3523fb8275c3a3d7be8f141eea19c6
Ankushjare <jareankush@gmail.com>
2021-10-24 19:30:10 +0530

    Welcome In Snakes&Ladder

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        const int START_POISTION = 0;
+        const int END_POSTION = 100;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("start postion is: " + START_POISTION);
+            Console.WriteLine("end postion is: " + END_POSTION);
+        }
+
+    }
+}
